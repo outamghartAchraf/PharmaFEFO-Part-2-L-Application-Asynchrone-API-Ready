5657df411dd300c04ed4ede628de962005fce506
outamghartAchraf <outamghart2000@gmail.com>
2026-06-16 10:36:23 +0100

    add function create and edit for create user and edit

--- src/Controller/web/UserController.php
+++ src/Controller/web/UserController.php
@@ -20,5 +20,32 @@ class UserController
         include __DIR__ . '/../../../views/templates/dashboard/users/index.php';
     }
 
+    public static function create()
+    {
+        Middleware::isAdmin();
 
+        $roles = RoleRepository::getAll();
+
+        include __DIR__ . '/../../../views/templates/dashboard/users/create.php';
+    }
+
+    public static function edit()
+    {
+        Middleware::isAdmin();
+
+        $id = (int) $_GET['id'];
+
+        $user  = UserRepository::getById($id);
+        $roles = RoleRepository::getAll();
+
+        include __DIR__ . '/../../../views/templates/dashboard/users/edit.php';
+    }
+
+    public static function logout()
+    {
+        session_destroy();
+
+        header('Location: index.php?action=login');
+        exit;
+    }
 }
