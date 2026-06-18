5786962c1dfc4e17102874a460b7c643cd381062
outamghartAchraf <outamghart2000@gmail.com>
2026-06-18 17:47:38 +0100

    create class ApiNotificationService

---
+++ src/Service/ApiNotificationService.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../Repository/BatchRepository.php';
+require_once __DIR__ . "/../middleware/RoleMiddleware.php";
+ 
+
+class ApiNotificationService
+{
+    public static function index()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        $expiredBatches = BatchRepository::getExpiredBatches();
+        $expiringBatches = BatchRepository::getExpiringBatches(90);
+
+        echo json_encode([
+            'success' => true,
+            'expired_batches' => $expiredBatches,
+            'expiring_batches' => $expiringBatches
+        ]);
+
+        exit;
+    }
+}
