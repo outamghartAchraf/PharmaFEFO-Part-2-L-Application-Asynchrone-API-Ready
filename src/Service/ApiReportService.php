6f239d86d4ce0be10b87c3f80410f722175509d2
outamghartAchraf <outamghart2000@gmail.com>
2026-06-18 17:49:35 +0100

    create class ApiReportService

---
+++ src/Service/ApiReportService.php
@@ -0,0 +1,72 @@
+<?php
+
+require_once __DIR__ . '/../Repository/ReportRepository.php';
+require_once __DIR__ . "/../middleware/RoleMiddleware.php";
+
+class ApiReportService
+{
+    public static function statistics()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        echo json_encode(
+            ReportRepository::getStatistics()
+        );
+
+        exit;
+    }
+
+    public static function stock()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        echo json_encode(
+            ReportRepository::getCurrentStockReport()
+        );
+
+        exit;
+    }
+
+    public static function expired()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        echo json_encode(
+            ReportRepository::getExpiredReport()
+        );
+
+        exit;
+    }
+
+    public static function expiringSoon()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        echo json_encode(
+            ReportRepository::getExpiringSoonReport()
+        );
+
+        exit;
+    }
+
+    public static function movements()
+    {
+        Middleware::isPharmacien();
+
+        header('Content-Type: application/json');
+
+        echo json_encode(
+            ReportRepository::getMovementsReport()
+        );
+
+        exit;
+    }
+}
